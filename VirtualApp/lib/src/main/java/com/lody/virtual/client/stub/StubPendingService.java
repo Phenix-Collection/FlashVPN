7ffc1496a1d618566f78db9d6cf958ea3ca4b6e5
Gary <test@test>
2016-12-29 01:07:26 +0800

    Add debug log

--- VirtualApp/lib/src/main/java/com/lody/virtual/client/stub/StubPendingService.java
+++ VirtualApp/lib/src/main/java/com/lody/virtual/client/stub/StubPendingService.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.IBinder;
 
 import com.lody.virtual.helper.component.BaseService;
+import com.lody.virtual.helper.utils.VLog;
 
 /**
  * @author Lody
@@ -17,6 +18,7 @@ public class StubPendingService extends BaseService {
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
+        VLog.d("StubPendingService", "onStartCommand " + intent.toString());
         startService(intent);
         stopSelf();
         return START_NOT_STICKY;
