6fb74c593da616da3df85e564bcd64fc88afa7ec
Gary <test@test>
2017-01-08 03:23:48 +0800

    Add FAQ link in feedback activity

--- VirtualApp/multiaccount/src/main/java/com/polestar/multiaccount/component/activity/FeedbackActivity.java
+++ VirtualApp/multiaccount/src/main/java/com/polestar/multiaccount/component/activity/FeedbackActivity.java
@@ -2,14 +2,21 @@ package com.polestar.multiaccount.component.activity;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
 import android.net.Uri;
 import android.os.Bundle;
 import android.text.Editable;
+import android.text.Spannable;
+import android.text.SpannableString;
+import android.text.TextPaint;
 import android.text.TextWatcher;
+import android.text.method.LinkMovementMethod;
+import android.text.style.ClickableSpan;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import com.polestar.multiaccount.R;
 import com.polestar.multiaccount.component.BaseActivity;
@@ -22,6 +29,7 @@ public class FeedbackActivity extends BaseActivity {
     private Context mContext;
     private EditText mEtFeedback;
     private Button mBtSubmit;
+    private TextView mGoFAQ;
 
 
     @Override
@@ -37,7 +45,28 @@ public class FeedbackActivity extends BaseActivity {
 
         mEtFeedback = (EditText) findViewById(R.id.et_feedback);
         mBtSubmit = (Button) findViewById(R.id.bt_submit);
+        mGoFAQ = (TextView) findViewById(R.id.tv_go_faq);
         mBtSubmit.setEnabled(false);
+
+        SpannableString spanText=new SpannableString(getString(R.string.feedback_go_faq));
+        spanText.setSpan(new ClickableSpan() {
+
+            @Override
+            public void updateDrawState(TextPaint ds) {
+                super.updateDrawState(ds);
+                ds.setColor(Color.BLUE);       //设置文件颜色
+                ds.setUnderlineText(true);      //设置下划线
+            }
+
+            @Override
+            public void onClick(View view) {
+                Intent intentToFAQ = new Intent(FeedbackActivity.this, FaqActivity.class);
+                startActivity(intentToFAQ);
+            }
+        }, spanText.length() - 4, spanText.length()-1, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+        mGoFAQ.setText(spanText);
+        mGoFAQ.setHighlightColor(Color.TRANSPARENT);
+        mGoFAQ.setMovementMethod(LinkMovementMethod.getInstance());
 
         mEtFeedback.addTextChangedListener(new TextWatcher() {
             @Override
