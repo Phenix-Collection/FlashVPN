3594bb00ed98e2c7ee1a97b390498e4f2966dbf0
Gary <test@test>
2017-01-15 00:24:40 +0800

    Add refContentProvider hook

---
+++ VirtualApp/lib/src/main/java/com/lody/virtual/client/hook/patchs/am/RefContentProvider.java
@@ -0,0 +1,39 @@
+package com.lody.virtual.client.hook.patchs.am;
+
+import com.lody.virtual.client.hook.base.Hook;
+import com.lody.virtual.helper.utils.VLog;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+
+/**
+ * Created by guojia on 2017/1/14.
+ */
+class RefContentProvider extends Hook {
+    private final static String TAG = "RefContentProvider";
+    @Override
+    public String getName() {
+        return "refContentProvider";
+    }
+
+    @Override
+    public Object call(Object who, Method method, Object... args) throws Throwable {
+        if (args[0] == null) {
+            VLog.logbug(TAG, "connection is null. return");
+            return null;
+        }else{
+            try{
+                return method.invoke(who, args);
+            }catch (InvocationTargetException e){
+                VLog.logbug(TAG, VLog.getStackTraceString(e));
+                return null;
+            }
+        }
+    }
+
+    @Override
+    public boolean isEnable() {
+        return isAppProcess();
+    }
+}
