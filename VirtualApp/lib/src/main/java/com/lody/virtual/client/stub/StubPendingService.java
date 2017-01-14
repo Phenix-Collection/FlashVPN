b473c65b7ee0ae8246e65933c60ee1671360c160
Gary <test@test>
2017-01-15 00:27:22 +0800

    Avoid dead loop to start StubPendingService
    1. clone What's call
    2. start and use What's call
    3. remove What's call

--- VirtualApp/lib/src/main/java/com/lody/virtual/client/stub/StubPendingService.java
+++ VirtualApp/lib/src/main/java/com/lody/virtual/client/stub/StubPendingService.java
@@ -1,5 +1,6 @@
 package com.lody.virtual.client.stub;
 
+import android.content.ComponentName;
 import android.content.Intent;
 import android.os.IBinder;
 
@@ -19,6 +20,13 @@ public class StubPendingService extends BaseService {
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
         VLog.d("StubPendingService", "onStartCommand " + intent.toString());
+        if (intent != null && intent.getComponent() != null) {
+            ComponentName cn  = intent.getComponent();
+            if (cn.getClassName().equals(StubPendingService.class.getName())){
+                stopSelf();
+                return START_NOT_STICKY;
+            }
+        }
         startService(intent);
         stopSelf();
         return START_NOT_STICKY;
