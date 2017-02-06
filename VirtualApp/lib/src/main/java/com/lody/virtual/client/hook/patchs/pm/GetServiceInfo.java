422b9d9eaa34e51b04d9a9375b917331103ba447
polestar.applab <test.test>
2017-02-06 22:30:09 +0800

    Fix getServiceInfo bug

--- VirtualApp/lib/src/main/java/com/lody/virtual/client/hook/patchs/pm/GetServiceInfo.java
+++ VirtualApp/lib/src/main/java/com/lody/virtual/client/hook/patchs/pm/GetServiceInfo.java
@@ -1,9 +1,11 @@
 package com.lody.virtual.client.hook.patchs.pm;
 
 import android.content.ComponentName;
+import android.content.pm.ServiceInfo;
 
 import com.lody.virtual.client.hook.base.Hook;
 import com.lody.virtual.client.ipc.VPackageManager;
+import com.lody.virtual.helper.utils.ComponentUtils;
 import com.lody.virtual.os.VUserHandle;
 
 import java.lang.reflect.Method;
@@ -33,7 +35,17 @@ import static android.content.pm.PackageManager.GET_DISABLED_COMPONENTS;
 			flags |= GET_DISABLED_COMPONENTS;
 		}
 		int userId = VUserHandle.myUserId();
-		return VPackageManager.get().getServiceInfo(componentName, flags, userId);
+		ServiceInfo info = VPackageManager.get().getServiceInfo(componentName, flags, userId);
+		if (info != null) {
+			return info;
+		}
+		info = (ServiceInfo) method.invoke(who, args);
+		if (info != null) {
+			if (getHostPkg().equals(info.packageName) || ComponentUtils.isSystemApp(info.applicationInfo)) {
+				return info;
+			}
+		}
+		return null;
 	}
 
 	@Override
