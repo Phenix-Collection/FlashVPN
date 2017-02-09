ce2b003b6ccd6be61be3e74281dc77e5711421cc
polestar.applab <test.test>
2017-02-09 23:23:54 +0800

    Set noisy=true in ActivityThread installProvider

--- VirtualApp/lib/src/main/java/mirror/android/app/ActivityThread.java
+++ VirtualApp/lib/src/main/java/mirror/android/app/ActivityThread.java
@@ -97,6 +97,6 @@ public class ActivityThread {
         if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.ICE_CREAM_SANDWICH_MR1) {
             return installProvider.call(mainThread, context, holder, providerInfo, false, true);
         }
-        return installProvider.call(mainThread, context, holder, providerInfo, false, true, true);
+        return installProvider.call(mainThread, context, holder, providerInfo, true, true, true);
     }
 }
