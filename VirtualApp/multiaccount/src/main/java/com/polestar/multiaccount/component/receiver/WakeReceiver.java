f0764ef58a592866b63abc152a17874eb79d13ae
Gary <test@test>
2017-05-14 16:17:04 +0800

    Add wake receiver

---
+++ VirtualApp/multiaccount/src/main/java/com/polestar/multiaccount/component/receiver/WakeReceiver.java
@@ -0,0 +1,19 @@
+package com.polestar.multiaccount.component.receiver;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+import com.polestar.multiaccount.utils.MLogs;
+
+/**
+ * Created by guojia on 2017/5/14.
+ */
+
+public class WakeReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        //
+        MLogs.logBug("Awake for " + intent);
+    }
+}
