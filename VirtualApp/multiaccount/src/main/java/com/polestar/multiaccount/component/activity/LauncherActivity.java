c62cffc158b32ab09bf39aa94456f894ae09f7f4
Gary <test@test>
2017-06-15 23:34:30 +0800

    UI: Remove start page

--- VirtualApp/multiaccount/src/main/java/com/polestar/multiaccount/component/activity/LauncherActivity.java
+++ VirtualApp/multiaccount/src/main/java/com/polestar/multiaccount/component/activity/LauncherActivity.java
@@ -46,9 +46,6 @@ import java.util.Map;
 public class LauncherActivity extends BaseActivity{
 
     private static boolean created;
-    private TextView termText ;
-    private TextView enterText;
-    private LinearLayout gmsSettingLayout;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -56,9 +53,6 @@ public class LauncherActivity extends BaseActivity{
         long time = System.currentTimeMillis();
         setContentView(R.layout.activity_mylauncher);
 //        mainLayout.setBackgroundResource(R.mipmap.launcher_bg_main);
-        termText = (TextView) findViewById(R.id.term_text);
-        enterText = (TextView) findViewById(R.id.enter_text);
-        gmsSettingLayout = (LinearLayout) findViewById(R.id.gms_setting_layout);
         FuseAdLoader.get(HomeFragment.SLOT_HOME_HEADER_NATIVE, this).loadAd(1, null);
         Handler handler = new Handler();
         handler.postDelayed(new Runnable() {
@@ -68,122 +62,19 @@ public class LauncherActivity extends BaseActivity{
             }
         },100);
 
-        //VirtualCore.get().waitForEngine();
-        if (PreferencesUtils.hasShownStartPage()) {
-            long delta = System.currentTimeMillis() - time;
-            handler.postDelayed(new Runnable() {
-                @Override
-                public void run() {
-                    enterHome();
-                }
-            }, 2000 - delta);
-        } else {
-            enterText.setVisibility(View.VISIBLE);
-            termText.setVisibility(View.VISIBLE);
-            gmsSettingLayout.setVisibility(View.VISIBLE);
-            CheckBox gmsCb= (CheckBox) gmsSettingLayout.findViewById(R.id.gms_cb);
-            DialogInterface.OnClickListener dialogListener = new DialogInterface.OnClickListener(){
-                @Override
-                public void onClick(DialogInterface dialogInterface, int i) {
-                    boolean orig = PreferencesUtils.isGMSEnable();
-                    switch (i) {
-                        case UpDownDialog.NEGATIVE_BUTTON:
-                            break;
-                        case UpDownDialog.POSITIVE_BUTTON:
-                            PreferencesUtils.setGMSEnable(!orig);
-                            VirtualCore.get().restart();
-                            boolean newStatus = PreferencesUtils.isGMSEnable();
-                            MTAManager.setGMS(LauncherActivity.this, newStatus, "startPage");
-                            if (newStatus) {
-                                Toast.makeText(LauncherActivity.this, getString(R.string.settings_gms_enable_toast), Toast.LENGTH_SHORT);
-                            } else {
-                                Toast.makeText(LauncherActivity.this, getString(R.string.settings_gms_disable_toast), Toast.LENGTH_SHORT);
-                            }
-                            break;
-                    }
-                    gmsCb.setChecked(PreferencesUtils.isGMSEnable());
-                }
-            };
-            gmsCb.setChecked(PreferencesUtils.isGMSEnable());
-            gmsCb.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    MTAManager.generalClickEvent(LauncherActivity.this, "start_page_gms_switch");
-                    if(PreferencesUtils.isGMSEnable()) {
-                        UpDownDialog.show(LauncherActivity.this, getString(R.string.delete_dialog_title), getString(R.string.settings_gms_disable_notice),
-                                getString(R.string.no_thanks), getString(R.string.yes), -1,
-                                R.layout.dialog_up_down, dialogListener).setOnCancelListener(new DialogInterface.OnCancelListener() {
-                            @Override
-                            public void onCancel(DialogInterface dialogInterface) {
-                                gmsCb.setChecked(PreferencesUtils.isGMSEnable());
-                            }
-                        });
-                    } else {
-                        UpDownDialog.show(LauncherActivity.this, getString(R.string.delete_dialog_title), getString(R.string.settings_gms_enable_notice),
-                                getString(R.string.no_thanks), getString(R.string.yes), -1,
-                                R.layout.dialog_up_down, dialogListener).setOnCancelListener(new DialogInterface.OnCancelListener() {
-                            @Override
-                            public void onCancel(DialogInterface dialogInterface) {
-                                gmsCb.setChecked(PreferencesUtils.isGMSEnable());
-                            }
-                        });
-                    }
-                }
-            });
-            SpannableString spanText=new SpannableString("Term of Service and Privacy Policy");
-            spanText.setSpan(new ClickableSpan() {
+        long delta = System.currentTimeMillis() - time;
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                enterHome();
+            }
+        }, 2000 - delta);
 
-                @Override
-                public void updateDrawState(TextPaint ds) {
-                    super.updateDrawState(ds);  //设置文件颜色
-                    ds.setColor(getResources().getColor(R.color.text_gray_dark));
-                    ds.setUnderlineText(true);      //设置下划线
-                }
-
-                @Override
-                public void onClick(View view) {
-                    MLogs.d("Spannable onclick");
-                    Intent intent = new Intent(LauncherActivity.this, WebViewActivity.class);
-                    intent.putExtra(WebViewActivity.EXTRA_TITLE, getString(R.string.settings_terms_of_service));
-                    intent.putExtra(WebViewActivity.EXTRA_URL, "file:///android_asset/term_of_service.html");
-                    startActivity(intent);
-                }
-            }, 0, 15, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
-            spanText.setSpan(new ClickableSpan() {
-
-                @Override
-                public void updateDrawState(TextPaint ds) {
-                    super.updateDrawState(ds);  //设置文件颜色
-                    ds.setColor(getResources().getColor(R.color.text_gray_dark));
-                    ds.setUnderlineText(true);      //设置下划线
-                }
-
-                @Override
-                public void onClick(View view) {
-                    MLogs.d("Spannable onclick");
-                    Intent intent = new Intent(LauncherActivity.this, WebViewActivity.class);
-                    intent.putExtra(WebViewActivity.EXTRA_TITLE, getString(R.string.settings_privacy_policy));
-                    intent.putExtra(WebViewActivity.EXTRA_URL, "file:///android_asset/privacy_policy.html");
-                    startActivity(intent);
-                }
-            }, 20, spanText.length(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
-            termText.setText(spanText);
-            termText.setHighlightColor(Color.TRANSPARENT);
-            termText.setMovementMethod(LinkMovementMethod.getInstance());
-            enterText.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    PreferencesUtils.setStartPageStatus(true);
-                    enterHome();
-                    if(!PreferencesUtils.isShortCutCreated() && !created) {
-                        PreferencesUtils.setShortCutCreated();
-                        createShortCut();
-                        created = true;
-                    }
-                }
-            });
+        if(!PreferencesUtils.isShortCutCreated() && !created) {
+            PreferencesUtils.setShortCutCreated();
+            createShortCut();
+            created = true;
         }
-
     }
 
 
