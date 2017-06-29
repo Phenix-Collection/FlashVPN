b0e0ab381ea982adda5c509992d0c7682e67b6ce
Gary <test@test>
2017-06-29 17:28:27 +0800

    Test: add notification test

--- MyApplication/app/src/main/java/com/polestar/myapplication/MApplication.java
+++ MyApplication/app/src/main/java/com/polestar/myapplication/MApplication.java
@@ -1,8 +1,12 @@
 package com.polestar.myapplication;
 
 import android.app.Application;
+import android.app.NotificationManager;
 import android.content.ContentProviderClient;
 import android.content.ContentResolver;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.util.Log;
 
 /**
@@ -15,14 +19,8 @@ public class MApplication extends Application {
     public void onCreate() {
         super.onCreate();
         Log.d(TAG, "MApplication onCreate");
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                ContentProviderClient test3 = getContentResolver().acquireContentProviderClient("com.facebook.katana.provider.PlatformProvider");
-                ContentProviderClient test4 = getContentResolver().acquireUnstableContentProviderClient("com.facebook.katana.provider.PlatformProvider");
-                Log.d(TAG, "test3 aclient: " + test3 + " test 4 " + test4);
-            }
-        }).start();
+
+
 //        ContentProviderClient test = this.getContentResolver().acquireContentProviderClient("com.facebook.katana.provider.PlatformProvider");
 //        ContentProviderClient test2 = this.getContentResolver().acquireUnstableContentProviderClient("com.facebook.katana.provider.PlatformProvider");
 //        Log.d(TAG, "test aclient: " + test + " test 2 " + test2);
--- MyApplication/app/src/main/java/com/polestar/myapplication/MainActivity.java
+++ MyApplication/app/src/main/java/com/polestar/myapplication/MainActivity.java
@@ -1,13 +1,19 @@
 package com.polestar.myapplication;
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.os.UserManager;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.app.NotificationCompat;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
@@ -27,6 +33,35 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                Bitmap btm = BitmapFactory.decodeResource(getResources(),
+                        R.mipmap.ic_launcher);
+                Intent intent = new Intent(MainActivity.this,
+                        MainActivity.class);
+                PendingIntent pendingIntent = PendingIntent.getActivity(
+                        MainActivity.this, 0, intent,
+                        PendingIntent.FLAG_CANCEL_CURRENT);
+                Notification noti = new NotificationCompat.Builder(
+                        MainActivity.this)
+                        .setSmallIcon(R.mipmap.ic_launcher)
+                        .setLargeIcon(btm)
+                        .setNumber(13)
+                        .setContentIntent(pendingIntent)
+                        .setStyle(
+                                new NotificationCompat.InboxStyle()
+                                        .addLine(
+                                                "M.Twain (Google+) Haiku is more than a cert...")
+                                        .addLine("M.Twain Reminder")
+                                        .addLine("M.Twain Lunch?")
+                                        .addLine(
+                                                "Google Play Celebrate 25 billion apps with Goo..")
+                                        .addLine(
+                                                "Stack Exchange StackOverflow weekly Newsl...")
+                                        .setBigContentTitle("6 new message")
+                                        .setSummaryText("mtwain@android.com"))
+                        .build();
+                NotificationManager mNotificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+                mNotificationManager.notify(0, noti);
+                mNotificationManager.notify(1, noti);
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
@@ -34,9 +69,9 @@ public class MainActivity extends AppCompatActivity {
         Intent test = registerReceiver(null, new IntentFilter(Intent.ACTION_BATTERY_CHANGED));
         Log.e("SPC", "ACTION_BATTERY_CHANGED " + test);
 
-        UserManager  um = (UserManager) getSystemService(Context.USER_SERVICE);
-        String name = um.getUserName();
-        Log.e("SPC", "UserName " + name);
+//        UserManager  um = (UserManager) getSystemService(Context.USER_SERVICE);
+//        String name = um.getUserName();
+//        Log.e("SPC", "UserName " + name);
     }
 
     @Override
