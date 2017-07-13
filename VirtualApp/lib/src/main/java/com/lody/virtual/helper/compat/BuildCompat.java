bab42a6b7417e6be2f5922892b8168c3380e8421
Gary <test@test>
2017-07-13 15:11:45 +0800

    Lib: fix Oreo check error

--- VirtualApp/lib/src/main/java/com/lody/virtual/helper/compat/BuildCompat.java
+++ VirtualApp/lib/src/main/java/com/lody/virtual/helper/compat/BuildCompat.java
@@ -10,7 +10,11 @@ public class BuildCompat {
 
     public static boolean isOreo() {
 
-        return (Build.VERSION.SDK_INT == 25 && Build.VERSION.PREVIEW_SDK_INT > 0)
-                || Build.VERSION.SDK_INT > 25;
+        try {
+            return (Build.VERSION.SDK_INT == 25 && Build.VERSION.PREVIEW_SDK_INT > 0)
+                    || Build.VERSION.SDK_INT > 25;
+        } catch (Exception e) {
+            return false;
+        }
     }
 }
