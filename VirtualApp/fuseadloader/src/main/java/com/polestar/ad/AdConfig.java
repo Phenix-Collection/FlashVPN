1a7577efd66650ae897123cbb8fc1faeb0418a21
Gary <test@test>
2017-12-18 00:10:38 +0800

    FuseAdloader: add Admob bannder

--- VirtualApp/fuseadloader/src/main/java/com/polestar/ad/AdConfig.java
+++ VirtualApp/fuseadloader/src/main/java/com/polestar/ad/AdConfig.java
@@ -1,5 +1,7 @@
 package com.polestar.ad;
 
+import com.google.android.gms.ads.AdSize;
+
 /**
  * Created by guojia on 2016/12/18.
  */
@@ -8,10 +10,34 @@ public class AdConfig {
     public String key;
     public String source;
     public long cacheTime;
+    public AdSize bannerAdSize;
+    public final static int BANNER = 0;
+    public final static int LARGE_BANNER = 1;
+    public final static int MEDIUM_RECTANGLE = 2;
+    public final static int SMART_BANNER = 3;
     public AdConfig(String source, String key, long cacheTime) {
         this.key = key;
         this.source = source;
         this.cacheTime = cacheTime;
+    }
+    public AdConfig(String source, String key, long cacheTime, int bannerType) {
+        this.key = key;
+        this.source = source;
+        this.cacheTime = cacheTime;
+        switch (bannerType) {
+            case BANNER:
+                this.bannerAdSize = AdSize.BANNER;
+                break;
+            case LARGE_BANNER:
+                this.bannerAdSize = AdSize.LARGE_BANNER;
+                break;
+            case MEDIUM_RECTANGLE:
+                this.bannerAdSize = AdSize.MEDIUM_RECTANGLE;
+                break;
+            case SMART_BANNER:
+                this.bannerAdSize = AdSize.SMART_BANNER;
+                break;
+        }
     }
     public String toString () {
         return "source: " + source + " key:" + key + " cache_time:" + cacheTime;
--- VirtualApp/fuseadloader/src/main/java/com/polestar/ad/AdConstants.java
+++ VirtualApp/fuseadloader/src/main/java/com/polestar/ad/AdConstants.java
@@ -24,6 +24,7 @@ public class AdConstants {
         public static final String AD_SOURCE_ADMOB_INTERSTITIAL = "ab_interstitial";
         public static final String AD_SOURCE_MOPUB_INTERSTITIAL = "mp_interstitial";
         public static final String AD_SOURCE_ADMOB_NAVTIVE_BANNER = "ab_native_banner";
+        public static final String AD_SOURCE_ADMOB_BANNER = "ab_banner";
         public static final String AD_SOURCE_VK = "vk";
     }
 }
