ec9a1d7dfbbb15cf27112b652a30720fbdf20595
Gary <test@test>
2018-06-02 00:43:08 +0800

    LibMultiple:  add messages in notification

--- VirtualApp/libmultiple/src/main/java/com/lody/virtual/client/stub/DaemonService.java
+++ VirtualApp/libmultiple/src/main/java/com/lody/virtual/client/stub/DaemonService.java
@@ -6,8 +6,10 @@ import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 import android.os.IBinder;
 
+import com.lody.virtual.R;
 import com.lody.virtual.client.core.VirtualCore;
 import com.lody.virtual.helper.utils.VLog;
 
@@ -68,7 +70,24 @@ public class DaemonService extends Service {
 
         @Override
         public int onStartCommand(Intent intent, int flags, int startId) {
-            startForeground(NOTIFY_ID, new Notification());
+            //  remoteViews = new RemoteViews(this.getPackageName(), R.layout.quick_switch_notification);
+            Notification notification;
+            if (Build.VERSION.SDK_INT > Build.VERSION_CODES.N_MR1) {
+                Intent start = new Intent();
+                start.addCategory(Intent.CATEGORY_LAUNCHER);
+                start.setAction(Intent.ACTION_MAIN);
+                start.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                start.setPackage(this.getPackageName());
+                Notification.Builder mBuilder =  new Notification.Builder(this);
+                mBuilder.setContentTitle(getString(R.string.daemon_notification_text))
+                        .setContentText(getString(R.string.daemon_notification_detail))
+                        .setSmallIcon(this.getResources().getIdentifier("ic_launcher", "mipmap", this.getPackageName()))
+                        .setContentIntent(PendingIntent.getActivity(this,0, start, 0));
+                notification = mBuilder.build();
+            } else {
+                notification = new Notification();
+            }
+            startForeground(NOTIFY_ID, notification);
             stopForeground(true);
             stopSelf();
             return super.onStartCommand(intent, flags, startId);
