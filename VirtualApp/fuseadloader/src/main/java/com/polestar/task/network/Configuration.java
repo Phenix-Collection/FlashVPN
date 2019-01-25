4aa14fd75487ffd39a7c30bade27090c7e03b4b3
huan zheng <huanzheng@huans-MacBook-Pro-2.local>
2019-01-25 15:07:09 +0800

    added aws server ip

--- VirtualApp/fuseadloader/src/main/java/com/polestar/task/network/Configuration.java
+++ VirtualApp/fuseadloader/src/main/java/com/polestar/task/network/Configuration.java
@@ -1,7 +1,7 @@
 package com.polestar.task.network;
 
 public class Configuration {
-    public static final String URL_PREFIX = "http://192.168.1.5:8000";
+    public static final String URL_PREFIX = "http://13.112.221.35";
 
     public static final int STATUS_OPEN = 1;
     public static final int STATUS_CLOSED = 2;
