06c43277ad78d5d2b8abbc8c5efdc799b5deb78b
gary <gray@test>
2019-03-22 11:36:34 +0800

    LibMultiple: fix getCacheQuotaBytes crash

--- VirtualApp/libmultiple/src/main/java/com/polestar/clone/client/hook/proxies/mount/MountServiceStub.java
+++ VirtualApp/libmultiple/src/main/java/com/polestar/clone/client/hook/proxies/mount/MountServiceStub.java
@@ -4,7 +4,11 @@ import android.os.IInterface;
 
 import com.polestar.clone.client.hook.base.Inject;
 import com.polestar.clone.client.hook.base.BinderInvocationProxy;
+import com.polestar.clone.client.hook.base.ReplaceLastPkgMethodProxy;
+import com.polestar.clone.client.hook.base.ReplaceUidMethodProxy;
 import com.polestar.clone.helper.compat.BuildCompat;
+
+import java.lang.reflect.Method;
 
 import mirror.RefStaticMethod;
 import mirror.android.os.mount.IMountService;
@@ -26,5 +30,21 @@ public class MountServiceStub extends BinderInvocationProxy {
         } else {
             return IMountService.Stub.asInterface;
         }
+
+        //getCacheSizeBytes
+        //getCacheQuotaBytes(convert(storageUuid), app.uid)
+
+//        long getCacheQuotaBytes(String volumeUuid, int uid) = 75;
+//        295    long getCacheSizeBytes(String volumeUuid, int uid) = 76;
+//        296    long getAllocatableBytes(String volumeUuid, int flags, String callingPackage) = 77;
+    }
+
+
+    @Override
+    protected void onBindMethods() {
+        super.onBindMethods();
+        addMethodProxy(new ReplaceLastPkgMethodProxy("getAllocatableBytes"));
+        addMethodProxy(new ReplaceUidMethodProxy("getCacheSizeBytes", 1));
+        addMethodProxy(new ReplaceUidMethodProxy("getCacheQuotaBytes", 1));
     }
 }
